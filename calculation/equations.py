9da3daf9e09fe23b557c316a231f98cf0fe92eda
simonari <vsimonari@gmail.com>
2023-01-11 16:16:50 +0700

    Added DOP853 integration method via scipy package

--- calculation/equations.py
+++ calculation/equations.py
@@ -1,6 +1,6 @@
 from numpy import array
 from numpy import sin
-from numpy import sum, power, sqrt
+from numpy import power, sqrt
 
 from constants_lib import gravitational_parameter as mu
 
@@ -9,13 +9,14 @@ def kepler_equation(eccentric_anomaly, mean_anomaly, eccentricity):
     return mean_anomaly + eccentricity * sin(eccentric_anomaly)
 
 
-def f(state):
+def f(t, state):
     """
     Right part of motion's differential equation.
     Args:
+            t: Time of given state
         state: (r, v) vector, where r - coordinates, v - velocities.
     """
-    radius = sqrt(sum(power(array([state[0], state[1], state[2]]), 2)))
+    radius = sqrt(power(state[0], 2) + power(state[1], 2) + power(state[2], 2))
 
     return array([
         state[3],
