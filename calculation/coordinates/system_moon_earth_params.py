cda6817ab7293a3dc5944c9654dc948fd34ee6d9
simonari <vsimonari@gmail.com>
2023-01-11 19:12:42 +0700

    Added module to calculate force caused by Earth

---
+++ calculation/coordinates/system_moon_earth_params.py
@@ -0,0 +1,10 @@
+from numpy import float64 as float
+from numpy import array
+
+
+e1e2 = (array([-.781828867, -.662735076, -.0189098618], dtype=float),
+        array([0.684636126, -.662034129, -.0303143777], dtype=float))
+
+a_m = float(384400.0)
+
+n_m = float(.229970839)
